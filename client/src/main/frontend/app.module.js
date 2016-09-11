bd4ba9000a3feaa58224f7b914288ad10aefa183
Stefan Dotti <stefan@dotti.at>
2016-09-11 11:49:38 +0200

    resize eingebaut

--- client/src/main/frontend/app.module.js
+++ client/src/main/frontend/app.module.js
@@ -1,9 +1,33 @@
 'use strict';
 
 // Define the module
-angular.module('rocnet-client-info-system', [
+var app = angular.module('rocnet-client-info-system', [
   'ngRoute',
+  'ngResize',
   'core',
   'display',
   'stationList',
 ]);
+app.controller('resize', ['$scope', function($scope) {
+    $scope.resized=function($event){
+        var w = $event.width;
+        var h = $event.height;
+        var e = document.querySelector('body');
+        var cw = 1024;
+        var ch = 768;
+        var dx = w/cw;
+        var dy = h/ch;
+
+        console.log("w = "+w+ ", h = "+h+", "+cw+", "+ch);
+        console.log(dx+", "+dy);
+
+        if (dx > dy) {
+            dx = dy;
+        }
+
+        console.log(dx);
+
+        e.style.transformOrigin = '50% 50%';
+        e.style.transform = 'scale('+dx+')';
+    }
+}]);
